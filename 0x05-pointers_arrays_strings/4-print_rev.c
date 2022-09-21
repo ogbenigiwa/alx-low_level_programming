417eecb64992bd2c5a8e91bde70647361633a47b
ogbenigiwa <nadewale91@gmail.com>
2022-09-21 05:24:43 +0100

    updates

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+/**
+ * print_rev - print string in reverse
+ * @s: string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
